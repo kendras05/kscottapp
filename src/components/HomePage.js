bbec9605cd10b15d5d1526e3e0c84abfa5981d4f
kendras05 <kendrapsy@gmail.com>
2021-10-09 08:08:58 -0700

    more css

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,11 +5,9 @@ import me from '../images/me.png';
 function HomePage() {
 	return (
 		<React.Fragment>
-		<div  styles={{ backgroundImage:`url(${me})` }}>
 		<div className="jumbotron">
 					<h1>Kendra Scott</h1>
 					<p>Jr. Software Developer. Looking for first fulltime opportunity! Thank you for being here.</p>
-		</div>
 		</div>
     <img src="https://www.dropbox.com/home/Resumes?preview=green.pn" class="img-fluid" alt="..." />
 		</React.Fragment>
