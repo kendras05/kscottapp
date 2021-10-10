9c4e634055a75327758711ba6b1ca860ee45ef6b
kendras05 <kendrapsy@gmail.com>
2021-10-10 14:03:13 -0700

    worked on the starting touches to about page. Added text descriptions to videos. Made some CSS changes

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -5,7 +5,20 @@ class AboutPage extends React.Component {
 		return (	
 			<React.Fragment>
 				<h2>About</h2>
-				<p>This app uses React.</p>
+				<p>I have a certification in full stack development from UNLV through DevPoint Labs, received in April 2020.
+						I am from San Bernardino, CA but I spent my teenage years in Las Vegas, NV. Both are home
+						but I spent the majority of my 20s in Arizona where I earned a Bachelors in Psychology from Arizona
+						State University. 
+            
+						I have worked in many industries such as a Digital Service specialist at Bank of America. I have also been
+						the resident IT support for mental health agencies. I eventually combined both of my passions of helping
+						people and technology when I worked as a Neurofeedback/Biofeedback Specialist for a plethora of mental
+						health agecies in Las Vegas. I performed the therapy for clients while also installing, maintaining and teaching
+						the employees on how to use both the software and hardware. 
+						
+						I am currently employed in the public sector working with the Clark County Government under the Department of
+						Family Services. 
+						</p>
 			</React.Fragment>
 		);
 	}
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -14,20 +14,35 @@ function HomePage() {
    
     <div className="App">
       <h1>My Projects</h1>
-			<h3>Stamina</h3>		
+			<h3>Stamina</h3>
+			<h5>This was built on Ruby on Rails and uses a webscraper to get return data.</h5>
       <YoutubeEmbed embedId="tPNadVOFsbQ" />
     </div>
   
     <div className="App">
 	  <h3>WiX Website</h3>
+		<h5>This is an ecommerce site that was built and designed by myself without a template. </h5>
       <YoutubeEmbed embedId="QP9bhcNyaqo" />
     </div>
 
     <div className="App">
 		<h3>Wordpress Website/Heavy CSS</h3>
+		<h5>This site required a lot of CSS in order to modify the the pages to meet the needs of the client.</h5>
       <YoutubeEmbed embedId="vQosG2nDE5c" />
     </div>
+
+    <div className="App">
+		<h1>"Other Sites I've Briefly Worked On"</h1>
+		</div>
+    
+    <div className="App">
+	  <h3>Wordpress Website</h3>
+		<h5>Used CSS to change some features on the first page. </h5>
+      <YoutubeEmbed embedId="rKSu5PjjKSA" />
+    </div>
+					
 		</React.Fragment>
+
 	);
 }
 
